7b11afcf52e6349a135bb77d522d240cc57a4436
shoarora <shoarora@stanford.edu>
2017-12-07 03:14:34 -0800

    friends flow

--- stage0/js/friends.js
+++ stage0/js/friends.js
@@ -1,49 +1,43 @@
 const electron = require('electron');
-const request = require('request');
 const url = require('url');
 const path = require('path');
 
-var numReps = 20;
-var needsRender = true;
-var mainWindow;
-var canvasHeight;
-var canvasWidth;
-var inSelectMode = false;
-var selected = 0;
+var div = document.getElementById('menu-buttons');
 
-function setup() {
-    mainWindow = electron.remote.getGlobal('sharedObj').mainWindow;
-    const size = mainWindow.getSize();
-    console.log(size);
-    canvasWidth = size[0];
-    canvasHeight = size[1];
-    var canvas = createCanvas(canvasWidth, canvasHeight);
-    canvas.parent('canvas-container');
-}
+function main() {
+    var index = 0;
+    function highlightOptions() {
 
-function draw() {
-
-}
-
-document.addEventListener('keydown', event => {
-    if (event.key === 'j' && !inSelectMode) {
-        inSelectMode = true;
-    }
-    if (event.key === 'j' && inSelectMode) {
-        if (selected === 0) {
-            // TODO go to thing
-        }
-        if (selected === 1) {
-            // TODO go to thing
+        for (let i=0; i < div.children.length; i++) {
+            if (i === index) {
+                console.log('changing', i);
+                div.children[i].style.backgroundColor = 'rgb(239, 103, 148)';
+                div.children[i].style.color = 'white';
+            } else {
+                div.children[i].style.backgroundColor = '';
+                div.children[i].style.color = 'rgb(186, 171, 180)';
+            }
         }
     }
-    if (event.keyCode === 37 && numReps > 1 && inSelectMode) {
-        // TODO move selector
-    }
-    if (event.keyCode === 39 && numReps < 30 && inSelectMode) {
-        // TODO move selector
-    }
-});
+    highlightOptions();
+    document.addEventListener('keydown', event => {
+        if (event.key === 'j' && index < 3) {
+            index++;
+        }
+        if (event.key === 'k' && index > 0) {
+            index--;
+        }
+        if (event.key === 'l') {
+            var mainWindow = electron.remote.getGlobal('sharedObj').mainWindow;
+            var pathToLoad = div.children[index].getAttribute('href');
+            mainWindow.loadURL(url.format({
+                pathname: path.join(__dirname, '..', pathToLoad),
+                protocol: 'file:',
+                slashes: true
+            }));
+        }
+        highlightOptions();
+    });
+}
 
-exports.setup = setup;
-exports.draw = draw;
+exports.main = main;
